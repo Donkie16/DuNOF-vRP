80f841cdbe619aa98cd64d8601b7b3d90406f525
dunof <36583033+dunof@users.noreply.github.com>
2018-02-24 21:56:03 -0300

    include rpchat

---
+++ resources/[dunof]/[customs]/rpchat/server/main.lua
@@ -0,0 +1,51 @@
+  AddEventHandler('chatMessage', function(source, name, message)
+      if string.sub(message, 1, string.len("/")) ~= "/" then
+          local name = GetPlayerName(source)
+		TriggerClientEvent("sendProximityMessage", -1, source, name, message)
+      end
+      CancelEvent()
+  end)
+  
+  RegisterCommand('me', function(source, args, user)
+      local name = GetPlayerName(source)
+      TriggerClientEvent("sendProximityMessageMe", -1, source, name, table.concat(args, " "))
+  end, false)
+
+  RegisterCommand('do', function(source, args, user)
+      local name = GetPlayerName(source)
+      TriggerClientEvent("sendProximityMessageDo", -1, source, name, table.concat(args, " "))
+  end, false)
+
+  RegisterCommand('roll', function(source, args, user)
+    local name = GetPlayerName(source)
+    num = math.random(0, 10)
+    TriggerClientEvent("sendProximityMessageRoll", -1, source, name, num, table.concat(args, " "))
+  end, false)
+
+  RegisterCommand('twitter', function(source, args, user)
+  	TriggerClientEvent('chatMessage', -1, "^0[^4TWITTER^0] (^5@" .. GetPlayerName(source) .. "^0)", {30, 144, 255}, table.concat(args, " "))
+  end, false)
+
+  RegisterCommand('frp', function(source, args, user)
+  	TriggerClientEvent('chatMessage', -1, "FORA DO RP | " .. GetPlayerName(source), {128, 128, 128}, table.concat(args, " "))
+  end, false)
+
+  RegisterCommand('ilegal', function(source, args, user)
+    TriggerClientEvent('chatMessage', -1, "^0[^4ILEGAL^0] ", {128, 128, 128}, table.concat(args, " "))
+  end, false)
+
+  RegisterCommand('ad', function(source, args, user)
+  	TriggerClientEvent('chatMessage', -1, "^1[ANUNCIO]: " .. GetPlayerName(source), {255,215,0}, table.concat(args, " "))
+  end, false)
+
+function stringsplit(inputstr, sep)
+	if sep == nil then
+		sep = "%s"
+	end
+	local t={} ; i=1
+	for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+		t[i] = str
+		i = i + 1
+	end
+	return t
+end
